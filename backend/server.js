504a2030e88ab2d73fab831e534122097ea06626
susangi-admin <susangi@mylinex.com>
2022-09-19 11:35:34 +0530

    build : add error handler to server.js file as middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000; // get port from env or not setup env get 5000 port
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : false}))
 
+app.use (errorHandler);
 //routes start
 
 //export goalRoutes
