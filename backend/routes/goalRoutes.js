daaba2a9b41cdd81838a16d86d7a22270f8d0f72
susangi-admin <susangi@mylinex.com>
2022-09-19 11:02:27 +0530

    change : change routes of goals to two lines in routes file

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {getGoals,setGoals,updateGoals,deleteGoals} = require('../controllers/goalController'); //import getGoals function from controller
+const {
+		getGoals,
+		setGoals,
+		updateGoals,
+		deleteGoals} = require('../controllers/goalController'); //import getGoals function from controller
 
-//goalRoutes show route
-router.get('/',getGoals);
-
-
-//goalRoutes create route
-router.post('/',setGoals);
-
-//goalRoutes update route
-router.put('/:id',updateGoals);
-
-//goalRoutes delete route
-router.delete('/:id',deleteGoals);
+//goalRoutes route
+router.route('/').get(getGoals).post(setGoals);
+router.route('/:id').put(updateGoals).delete(deleteGoals);
 
 module.exports = router;
