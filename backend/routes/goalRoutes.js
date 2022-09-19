0e7adcc41279332eeb09666047c29fbf0ab32371
susangi-admin <susangi@mylinex.com>
2022-09-19 10:40:02 +0530

    build : build routes for crud oppertions

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-//goalRoutes get route
+//goalRoutes show route
 router.get('/',(req,res) => {
 	res.status(200).json({message : 'GET GOALS'});
-})
+});
+
+
+//goalRoutes create route
+router.post('/',(req,res) => {
+	res.status(200).json({message : 'SET GOALS'});
+});
+
+//goalRoutes update route
+router.put('/:id',(req,res) => {
+	res.status(200).json({message : `Update GOALS ${req.params.id}`});
+});
+
+//goalRoutes delete route
+router.delete('/:id',(req,res) => {
+	res.status(200).json({message : `Delete GOALS ${req.params.id}`});
+});
 
 module.exports = router;
