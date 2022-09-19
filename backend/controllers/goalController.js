d246c7a16824c112188c1b2358f91b0d684d54ad
susangi-admin <susangi@mylinex.com>
2022-09-19 14:36:22 +0530

    build : build controller functions as async (beacuse mongodb have promise)

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,7 @@
 //@desc show goals function
 //@route GET /api/goals
 //@access Private
-const getGoals = (req,res) => {
+const getGoals = async (req,res) => {
 	res.status(200).json({
 		message : `Get goals`
 	});
@@ -10,7 +10,7 @@ const getGoals = (req,res) => {
 //@desc create goals function
 //@route POST /api/goals
 //@access Private
-const setGoals = (req,res) => {
+const setGoals = async (req,res) => {
 	if(!req.body.text){
 		res.status(400); // client bad request
 		throw new Error('Please add a text field'); // 	express error handler
@@ -21,7 +21,7 @@ const setGoals = (req,res) => {
 //@desc update goals function
 //@route PUT /api/goals/:id
 //@access Private
-const updateGoals = (req,res) => {
+const updateGoals = async (req,res) => {
 	res.status(200).json(
 		{message : `Update GOALS ${req.params.id}`}
 	);
@@ -30,7 +30,7 @@ const updateGoals = (req,res) => {
 //@desc delete goals function
 //@route DELETE /api/goals/:id
 //@access Private
-const deleteGoals = (req,res) => {
+const deleteGoals = async (req,res) => {
 	res.status(200).json(
 		{message : `Delete GOALS ${req.params.id}`}
 	);
