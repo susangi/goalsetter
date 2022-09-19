43f5b8ce14ce087e798c5bab3af248f6398c624c
susangi-admin <susangi@mylinex.com>
2022-09-19 11:14:51 +0530

    build : build check request body isset and sent responce status => 400 and message

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -11,7 +11,9 @@ const getGoals = (req,res) => {
 //@route POST /api/goals
 //@access Private
 const setGoals = (req,res) => {
-	console.log(req.body);
+	if(!req.body.text){
+		res.status(400).json({message : `Please add a text field`}); // client bad request
+	}
 }
 
 
