f65642db8fbaaaaa7b767750f294fcf6e98e2556
susangi-admin <susangi@mylinex.com>
2022-09-19 11:39:02 +0530

    build : add error handler to server.js file as middleware

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : false}))
 
-app.use (errorHandler);
 //routes start
 
 //export goalRoutes
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+//error handler middleware
+app.use (errorHandler);
 
 app.listen(port, () => console.log(`Server start on port ${port}`));
