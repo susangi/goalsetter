4c9ba3b11ab09b563c3d2245c0569d1f01dacb09
susangi-admin <susangi@mylinex.com>
2022-09-20 11:25:41 +0530

    build : build model for goal and getGoal from db

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,15 @@
 const asyncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalModel')
+
 //@desc show goals function
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req,res) => {
-	res.status(200).json({
-		message : `Get goals`
-	});
+	
+	const goals = await Goal.find();
+	
+	res.status(200).json(goals);
 });
 
 //@desc create goals function
