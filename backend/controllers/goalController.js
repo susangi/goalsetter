4304f99bbfb3732945832f27a22a328c7d15385d
susangi-admin <susangi@mylinex.com>
2022-09-20 12:18:27 +0530

    build : build updated goal method

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -32,6 +32,19 @@ const setGoals = asyncHandler(async (req,res) => {
 //@route PUT /api/goals/:id
 //@access Private
 const updateGoals = asyncHandler( async (req,res) => {
+	const goal = await Goal.findById(req.params.id)
+
+	if(!goal){
+		res.status(400)
+		throw new Error('Goal node have found');
+	}
+
+	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+		new : true,	
+	})
+
+	res.status(200).json(updatedGoal)
+
 	res.status(200).json(
 		{message : `Update GOALS ${req.params.id}`}
 	);
