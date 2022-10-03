8f8b651e06b69af88df80c7c9a64d9572aa668a8
susangi-admin <susangi@mylinex.com>
2022-10-03 13:42:48 +0530

    build : build login and registration

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,68 @@
+import { useState, UseEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
 function Login() {
+  const [fromData, setFromData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = fromData;
+
+  const onChange = (e) => {
+    setFromData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+          <p>Logging and start setting goals</p>
+        </h1>
+      </section>
+
+      <section className="form">
+        <from onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </from>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import { useState, UseEffect } from "react";
+import { FaUser } from "react-icons/fa";
 
 function Register() {
+  const [fromData, setFromData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = fromData;
+
+  const onChange = (e) => {
+    setFromData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      Register
-    </div>
-  )
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser />
+          Register
+          <p>Please create an account</p>
+        </h1>
+      </section>
+
+      <section className="form">
+        <from onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Enter your name"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Confirm Password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </from>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
